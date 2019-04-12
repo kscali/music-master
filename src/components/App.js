35385938d26980cd5e910747b49de52f40c16af8
koy <kosatun@aol.com>
2019-04-11 23:18:15 -0700

    added new directory src and dist and created .babelrc

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  
+  render() {
+     return (
+      <div>
+        React App
+      </div>
+    )
+  }
+}
+
+
+export default App; 
