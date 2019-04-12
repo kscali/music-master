78ad4791e81f3547d90e2fbe307a8a8b7a02fdee
koy <kosatun@aol.com>
2019-04-12 00:09:09 -0700

    added a search bar and event handlers to App.js

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -24055,13 +24055,17 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
 var App =
 /*#__PURE__*/
@@ -24069,15 +24073,77 @@ function (_React$Component) {
   _inherits(App, _React$Component);
 
   function App() {
+    var _getPrototypeOf2;
+
+    var _this;
+
     _classCallCheck(this, App);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(App)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      artistQuery: "",
+      artist: null,
+      tracks: null
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "updateQuery", function (e) {
+      _this.setState({
+        artistQuery: e.target.value
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "searchArtist", function () {
+      var artist = _this.state.artistQuery;
+      fetch("".concat(API_ADDRESS, "/artist/").concat(artist)).then(function (res) {
+        return res.json();
+      }).then(function (json) {
+        if (json.artists.total > 0) {
+          var _artist = json.artists.items[0];
+
+          _this.setState({
+            artist: _artist
+          });
+
+          fetch("".concat(API_ADDRESS, "/artist/").concat(_artist.id, "/top-tracks")).then(function (res) {
+            return res.json();
+          }).then(function (json) {
+            return _this.setState({
+              tracks: json.tracks
+            });
+          }).catch(function (error) {
+            return alert(error.message);
+          });
+        }
+      }).catch(function (error) {
+        return alert(error.message);
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "handleKeyPress", function (e) {
+      if (e.key === "Enter") {
+        _this.searchArtist();
+      }
+    });
+
+    return _this;
   }
 
   _createClass(App, [{
     key: "render",
     value: function render() {
-      return _react.default.createElement("div", null, "React App");
+      console.log("this.state", this.state);
+      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Music Master"), _react.default.createElement("input", {
+        onChange: this.updateQuery,
+        onKeyPress: this.handleKeyPress,
+        placeholder: " Search for an Artist"
+      }), _react.default.createElement("button", {
+        onClick: this.searchArtist
+      }, "Search"));
     }
   }]);
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 import React from 'react';
 
+const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
+
 class App extends React.Component {
+  state = { 
+    artistQuery: "", 
+    artist: null,
+    tracks: null
+  };
+
+  updateQuery = e => {
+    this.setState({ artistQuery: e.target.value })
+  }
+
+  searchArtist = () => {
+    let artist = this.state.artistQuery;
+
+    fetch(`${API_ADDRESS}/artist/${artist}`)
+    .then(res => res.json())
+    .then(json => {
+       if (json.artists.total > 0) {
+        const artist = json.artists.items[0];
+
+        this.setState({ artist })
+
+        fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+        .then(res => res.json())
+        .then(json => this.setState({ tracks: json.tracks }))
+        .catch(error => alert(error.message));
+        
+      }
+    })
+    .catch(error => alert(error.message));
+  }
+
+  handleKeyPress = e => {
+    if (e.key === "Enter") {
+      this.searchArtist(); 
+    }
+  }
   
   render() {
+    console.log("this.state", this.state);
      return (
       <div>
-        React App
+        <h2>Music Master</h2>
+        <input onChange={ this.updateQuery } 
+        onKeyPress={ this.handleKeyPress }
+        placeholder=" Search for an Artist" />
+        <button onClick={ this.searchArtist }>Search</button>
       </div>
     )
   }
